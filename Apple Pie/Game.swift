f4191cf2a6af623d9163fd90d2e2668ab20a8e6c
Stas Borovcov <sborovcov@gmail.com>
2020-12-24 23:28:04 +0700

    Implementied correct word updater

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -7,9 +7,39 @@
 
 import Foundation
 
-import Foundation
-
 struct Game{
     var word: String
     var incorrectMovesRemaining: Int
+    //private - никому не видно этот массив
+    //fileprivate - доступ только внутри этого файла
+    fileprivate var guessedLetters: [Character] = [] //или [Character]() или [Character].init() - это таже самая инициализация массива
+    
+    //пишем свой конструктор, потому что есть приватная переменная и контруктор поумолчанию не может работать, так как две переменные нельзя инициализировать(их же три), но и приватноую переменную мы не можем инициализировать
+    init(word: String, incorrectMovesRemaining: Int){
+        self.word = word
+        self.incorrectMovesRemaining = incorrectMovesRemaining
+    }
+    
+    var guessedWord: String{
+        var wordToShow = ""
+        
+        for letter in word{
+            if guessedLetters.contains(Character(letter.lowercased())){
+                wordToShow += String(letter)
+            }else{
+                wordToShow += "_"
+            }
+        }
+        return wordToShow
+    }
+    
+    //mutating - значит функция меняет переменные структуры
+    mutating func playerGuessed(letter: Character){
+        let lowerCaseLetter = Character(letter.lowercased())
+        guessedLetters.append(lowerCaseLetter)
+        if !word.lowercased().contains(lowerCaseLetter){
+            incorrectMovesRemaining -= 1
+        }
+    }
 }
+
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -29,11 +29,21 @@ class ViewController: UIViewController {
         updateUI()
     }
     
+    func updatecorrectWordLabel(){
+        var displayWord = [String]()
+        for letter in currentGame.guessedWord{
+            displayWord.append(String(letter))
+        }
+        correctWordLabel.text = displayWord.joined(separator: " ")
+    }
+    
     func updateUI(){
         let movesRemaining = currentGame.incorrectMovesRemaining
-        let treeName = "Tree\(movesRemaining < 8 ? movesRemaining : 7)"
+        let treeName = "Tree\(movesRemaining < 0 ? 0 : movesRemaining < 8 ? movesRemaining : 7)"
         treeImageView.image = UIImage(named: treeName)
-        scoreLabel.text = "Выйгрыши: \(totalWins), проигрыши: \(totalLosses)"
+        scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLosses)"
+        //correctWordLabel.text = currentGame.guessedWord // вместо этого сделаем функцию с разделителями, хотя можно просто при формировании guessedWord добавлять пробелы при выводе подчеркивания. Но это лучше, так как мы изменяем строку только при выводе на экран
+        updatecorrectWordLabel()
     }
     
     override func viewDidLoad() {
@@ -44,6 +54,9 @@ class ViewController: UIViewController {
     // MARK: - IB Action
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
+        let letter = sender.title(for: .normal)!
+        currentGame.playerGuessed(letter: Character(letter))
+        updateUI()
     }
 }
 
